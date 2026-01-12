2c8b17159b914197572d0349ff3dff95925ccab2
razenskaya <158623889+razenskaya@users.noreply.github.com>
2026-01-12 03:18:54 +0300

    Create ohwell.js

---
+++ ohwell.js
@@ -0,0 +1,20 @@
+(()=>{const LOGO = String.raw`
+__webpack_require__(/*! P */ 420) && __webpack_exports__=__webpack_exports__||{};   __webpack_require__(/*! R */ 421);   (0,__webpack_require__(/*! O */ 422).O)();   __webpack_require__(/*! S */ 423);   __webpack_require__(/*! P */ 424);   __webpack_require__(/*! E */ 425);   __webpack_require__(/*! R */ 426);   __webpack_require__(/*! I */ 427);   __webpack_require__(/*! T */ 428);   __webpack_require__(/*! Y */ 429);
+
+PPPPPPPPPPPPPPPPPPPPPPPPPPPP   RRRRRRRRRRRRRRRRRRRRRRRR     OOOOOOOOOOOOOOOOOOOO     SSSSSSSSSSSSSSSSSSSSSS   PPPPPPPPPPPPPPPPPPPPPPPP   EEEEEEEEEEEEEEEEEEEEEEEE   RRRRRRRRRRRRRRRRRRRRRRRR   IIIIIIIIIIIIIIIIIIIIIII   TTTTTTTTTTTTTTTTTTTTTTTT   YYYYYYYYYYYYY       YYYYY
+PPPPPPPPPPPPPPPPPPPPPPPPPPPP   RRRRRRRRRRRRRRRRRRRRRRRR   OOOOOOOOOOOOOOOOOOOOOO   SSSSSSSSSSSSSSSSSSSSSSS   PPPPPPPPPPPPPPPPPPPPPPPP   EEEEEEEEEEEEEEEEEEEEEEEE   RRRRRRRRRRRRRRRRRRRRRRRR   IIIIIIIIIIIIIIIIIIIIIII   TTTTTTTTTTTTTTTTTTTTTTTT   YYYYYYYYYYYYY       YYYYY
+PPPPPPPPPP  __wreq__(1)  PPP   RRRR  __wreq__(2)  RRRRR   OOOO  __wreq__(3)  OOOO   SSS  __webpack_exports__    PPPPPPPPP  __wreq__  PPP   EEEE  __wreq__(7)  EEEE   RRRR  __wreq__(8)  RRRR          IIIII             TTTTTT                 YYYYYYYYYYYYYYYYYYYYY
+PPPPPPPPPP  module.exports PPP  RRRR  exports.__es RR      OOOO  chunkId=0  OOOO    SSSSSSSSSSSSSSSSSSSSSSS   PPPPPPPPPPPPPPPPPPPPPPPP   EEEEEEEEEEEEEEEEEEEEEEEE   RRRRRRRRRRRRRRRRRRRRRRRR          IIIII             TTTTTT                     YYYYYYYYYYYYYYY
+PPPPPPPPPP  __webpack_require__ PP   RRRRRRRRRRRRRRRR      OOOO  __webpack_chunk__ OOO    SSSSSSSSSSSSSSSSSSS   PPPPPPPPPPPPPPPPPPPPPPPP   EEEEEEEEEEEEEEEEEEEEEEEE   RRRRRRRRRRRRRRRRRR  RR          IIIII             TTTTTT                        YYYYYYYYYYY
+PPPPPPPPPP  .d(exports,"x",fn)  PP   RRRR  r=__wreq__      OOOO  Promise.resolve() OOO             __wreq__(9)   PPPP  __wreq__(10)  PPPP   EEEE  (0,eval)(src)  EEEE   RRRR  r.m[id].call RR          IIIII             TTTTTT                           YYYYYYY
+PPPPPPPPPP  /******/(()=>{})    PP   RRRR   __webpack_c__  OOOOOOOOOOOOOOOOOOOOOO   SSSSSSSSSSSSSSSSSSSSSSSS   PPPP  __webpack_hash__ PPPP   EEEEEEEEEEEEEEEEEEEEEEEE   RRRR   __webpack_runtime__ RR   IIIIIIIIIIIIIIIIIIIIIII   TTTTTT                           YYYYYYY
+PPPPPPPPPP  /******/  cache[id] PP   RRRR   __webpack_ids  OOOOOOOOOOOOOOOOOOOO     SSSSSSSSSSSSSSSSSSSSSSS    PPPP  __wreq__.n(mod)  PPPP   EEEEEEEEEEEEEEEEEEEEEEEE   RRRR   __wreq__.e(chunk)  RR   IIIIIIIIIIIIIIIIIIIIIII   TTTTTT                           YYYYYYY
+
+/* chunk loader noise */
+(()=>{var i=0;var e=(t)=>t&&t.__esModule?t:{default:t};
+var __webpack_chunk_load__=function(c){return Promise.all([c]).then(()=>c)};
+var __webpack_get_script__=(u)=>new Promise((r,j)=>{var s=document.createElement("script");s.src=u;s.onload=r;s.onerror=j;document.head.appendChild(s)});
+/* hashed runtime */
+var __webpack_hash__="a9f3c0b2d1e4f7";for(;i<3;i++)void(i^0x2a);
+})();
+`;if (typeof module !== "undefined" && module.exports) module.exports = LOGO;})();
